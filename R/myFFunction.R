ae53adb3a7f601360b97eadd2ae0335c09addce6
Jason Myers <Jmyers9595@gmail.com>
2020-06-15 22:17:43 -0700

    first add

---
+++ R/myFFunction.R
@@ -0,0 +1,12 @@
+#' Square function
+#'
+#' @param x takes vector and return vector squared
+#'
+#' @return vector of squared components
+#' @export
+#'
+#' @examples x = 1:30 myF(x)
+myF = function(x)
+{
+  x^2
+}
